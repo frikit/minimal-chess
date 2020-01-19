e31755376847dd1e74b9fff53a50f48c6b0e7664
frikit <osipovvictor1994@gmail.com>
2020-01-19 10:51:16 +0000

    add valid move test for king

--- src/test/kotlin/org/github/home/chess/logic/KingStrategyTest.kt
+++ src/test/kotlin/org/github/home/chess/logic/KingStrategyTest.kt
@@ -6,11 +6,20 @@ import org.junit.jupiter.api.Test
 
 internal class KingStrategyTest {
 
+    private val kingMoveCeil = Resource.getResource("king-valid-move.txt")
     private val kingMoveAliasCeil = Resource.getResource("king-invalid-move-alias-location.txt")
     private val kingMoveDownTwoCeil = Resource.getResource("king-invalid-move-down.txt")
     private val kingMoveUpTwoCeil = Resource.getResource("king-invalid-move-up.txt")
     private val kingMoveDiagonalTwoCeil = Resource.getResource("king-invalid-move-diagonal.txt")
 
+
+    @Test
+    fun `test valid move location`() {
+        val game = Main.playGame(kingMoveCeil)
+        val curr = game.moveHistory.size
+        val expected = Resource.readLines(kingMoveCeil).size
+        assert(curr == expected) { "Found in file $expected != $curr from history" }
+    }
 
     @Test
     fun `test valid move location but alias already there`() {
