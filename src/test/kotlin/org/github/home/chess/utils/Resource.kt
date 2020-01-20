eb840855a328441c5c1dcb7f77d20bdcb6a5091f
frikit <osipovvictor1994@gmail.com>
2020-01-20 17:35:54 +0000

    make get resource utils class more generic

--- src/test/kotlin/org/github/home/chess/utils/Resource.kt
+++ src/test/kotlin/org/github/home/chess/utils/Resource.kt
@@ -1,15 +1,16 @@
 package org.github.home.chess.utils
 
 import java.io.File
+import java.nio.file.Paths
 
 object Resource {
-    private val projFolder = "/Users/frikit/IdeaProjects/minimal-chess/src/main/resources/"
+    private val projFolder = Paths.get("").toAbsolutePath().toString() + "/src/main/resources/"
 
     fun getResource(fileName: String, folder: String = "base/"): String {
         return File(projFolder + folder + fileName).absolutePath
     }
 
-    fun readLines(path: String, folder: String = ""): List<String> {
+    fun readLines(path: String): List<String> {
         return File(path).readLines()
     }
 }
